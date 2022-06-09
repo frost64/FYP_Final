6357f813a52c41669902e925c95adb8764638ffd
Ali-shopdev <ali.riaz@shopdev.co>
2022-06-09 05:00:40 +0500

    add filer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,79 @@
 //--
-import data2 from "./Dataset.json";
+import objdata from "./Dataset.json";
 //--
 import "./App.css";
 import data from "./all_brands_export.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
-  
-  let array2 =[];
-  array2.push(data2);
-
-  // const [name, setName] = useState("");
-  // const [foundUsers2, setFoundUsers] = useState(array2[0].results);
-
-
   let arra = [];
   arra.push(data);
   let count = -1;
-
-  
+  const color = ["#8c8cf3", "#d7a954", "#a1f9a1", "pink"];
   const [name, setName] = useState("");
   const [foundUsers, setFoundUsers] = useState(arra[0].results);
+  const [displayChecked, setdisplayChecked] = useState(false);
+  const [batteryChecked, setbatteryChecked] = useState(false);
+  const [cameraChecked, setcameraChecked] = useState(false);
+  const [storageChecked, setstorageChecked] = useState(false);
+  const [data2, setdata2] = useState(objdata);
+  useEffect(() => {
+    let newdata = [];
 
-  console.log('Array2',array2[0])
-  //-- 
-  
-    
-  //--
+    if (cameraChecked) {
+      console.log("camera is cheaced");
+      newdata = data2
+        .sort((a, b) =>
+          a.camera_sum > b.camera_sum ? 1 : b.camera_sum > a.camera_sum ? -1 : 0
+        )
+        .reverse();
+      setdata2(newdata);
+    }
 
-  console.log(arra[0].results);
-  const color = ["#8c8cf3", "#d7a954", "#a1f9a1", "pink"];
+    if (displayChecked) {
+      console.log("display is cheaced");
+      newdata = data2
+        .sort((a, b) =>
+          a.display_sum > b.display_sum
+            ? 1
+            : b.display_sum > a.display_sum
+            ? -1
+            : 0
+        )
+        .reverse();
+
+      setdata2(newdata);
+    }
+    if (storageChecked) {
+      console.log("storage is cheaced");
+
+      newdata = data2
+        .sort((a, b) =>
+          a.storage_sum > b.storage_sum
+            ? 1
+            : b.storage_sum > a.storage_sum
+            ? -1
+            : 0
+        )
+        .reverse();
+
+      setdata2(newdata);
+    }
+    if (batteryChecked) {
+      console.log("batter is cheaced");
+
+      newdata = data2
+        .sort((a, b) =>
+          a.battery_sum > b.battery_sum
+            ? 1
+            : b.battery_sum > a.battery_sum
+            ? -1
+            : 0
+        )
+        .reverse();
+      setdata2(newdata);
+    }
+  }, [batteryChecked, storageChecked, cameraChecked, displayChecked]);
+
   const filter = (e) => {
     const keyword = e.target.value;
 
@@ -45,9 +90,8 @@ function App() {
     }
 
     setName(keyword);
+  };
 
-  
-    }
   return (
     <div className="App" style={{ backgroundColor: "#d0d9d9" }}>
       <h1 className="mainHeading">Mobile Analyzer</h1>
@@ -59,7 +103,7 @@ function App() {
             <input
               style={{
                 marginLeft: "15px",
-                height: "15px",
+                height: "25px",
                 alignSelf: "center",
               }}
               type="search"
@@ -69,50 +113,75 @@ function App() {
               placeholder="Filter"
             />
           </div>
+
           <div className="qualities">
             <h2>Qualities</h2>
             <div style={{ marginLeft: "20px" }}>
               <div>
-                <input type="checkbox" />
+                <input
+                  id="display"
+                  // checked={displayChecked}
+                  onChange={() => {
+                    setdisplayChecked(!displayChecked);
+                  }}
+                  type="checkbox"
+                />
+
                 <label>Display</label>
               </div>
               <div>
-                <input type="checkbox" />
+                <input
+                  id="camera"
+                  // checked={cameraChecked}
+                  onChange={() => {
+                    setcameraChecked(!cameraChecked);
+                  }}
+                  type="checkbox"
+                />
+
                 <label>Camera</label>
               </div>
               <div>
-                <input type="checkbox" />
+                <input
+                  id="storage"
+                  // checked={storageChecked}
+                  onChange={() => {
+                    setstorageChecked(!storageChecked);
+                  }}
+                  type="checkbox"
+                />
                 <label>Storage</label>
               </div>
               <div>
-                <input type="checkbox" />
+                <input
+                  id="battery"
+                  // checked={batteryChecked}
+                  onChange={() => {
+                    setbatteryChecked(!batteryChecked);
+                  }}
+                  type="checkbox"
+                />
                 <label>Battery</label>
               </div>
-              
-            
             </div>
-            <div style={{ textAlign: "center" }}>
-              <button style={{ cursor: "pointer" }}>Submit</button>
+            {/* <div style={{ textAlign: "center" }}>
+              <button style={{ cursor: "pointer" }} onClick={Submitform}>
+                Submit
+              </button>
+            </div> */}
+
+            <div className="abc">
+              <label>
+                Results: <br></br>
+              </label>
+              {data2.slice(0, 5).map((x1) => (
+                <span>
+                  <ul>
+                    <li>{x1.Name}</li>
+                  </ul>
+                </span>
+              ))}
             </div>
-
-
-            <div class = "abc">
-            <label>Results: <br></br></label>
-            {array2[0].slice(0,5).map((x1) =>(
-              <span> 
-                <ul>
-                  <li>
-                    {x1.Name}
-                  </li>
-                </ul>
-              </span>
-
-            ))}
-              
-              
-
-            </div>
-
           </div>
         </div>
         <div className="rightSide" style={{ padding: "2%" }}>
